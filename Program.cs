2700df2960cac2588667784b458f5bf7749abee5
Lars-Peter Olsen <lars5468@student.eadania.dk>
2016-05-07 12:21:43 +0200

    GameWorld Change

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace _1YearProject
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = GameWorld.Instance)
                 game.Run();
         }
     }
