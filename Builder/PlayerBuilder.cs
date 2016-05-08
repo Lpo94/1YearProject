01ffd517964b1f992810d5d37c1a14bd1400022f
Andreas Lahn Mittet <andremit@hotmail.com>
2016-05-08 11:32:01 +0200

    blargh

---
+++ Builder/PlayerBuilder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using _1YearProject.Components;
+using _1YearProject.Interfaces;
+
+namespace _1YearProject
+{
+    class PlayerBuilder : IBuilder
+    {
+        private GameObject gameObject = new GameObject();
+
+        public GameObject GetResult()
+        {
+            return gameObject;
+        }
+
+        public void BuildGameObject(Vector2 position)
+        {
+            gameObject.AddComponent(new SpriteRenderer(gameObject, "HeroSheet", 1));
+
+            gameObject.AddComponent(new Player(gameObject));
+
+            gameObject.AddComponent(new Animator(gameObject));
+
+            gameObject.AddComponent(new Collider(gameObject));
+        }
+    }
+}
